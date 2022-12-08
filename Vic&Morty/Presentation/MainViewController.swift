4ff5f136c02eccb92ae6e5de2e15b431b448adc3
Victor Chang <victor.chang@mercadolibre.com>
2022-12-08 11:05:13 -0300

    initial commit

---
+++ Vic&Morty/Presentation/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Vic&Morty
+//
+//  Created by Victor Chang on 08/12/2022.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
